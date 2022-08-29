571bf276ea042576058a6d02dd981dd7325b1c59
Daniel Kemen <dk@software-design.de>
2022-08-29 15:12:31 +0200

    Remove uploads from GIT

--- GitlabCustomerView/main/settings.py
+++ GitlabCustomerView/main/settings.py
@@ -114,8 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = 'uploads/'
 
+STATIC_URL = 'static/'
 LOGIN_URL = '/'
 
 # Default primary key field type
