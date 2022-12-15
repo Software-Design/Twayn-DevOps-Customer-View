c8ff531c77c98c3f6947dfea30bb2f189a16344b
André Hagemann <ah@software-design.de>
2022-12-15 16:24:07 +0100

    some changes to markdown.py and wiki

--- DevOpsCustomerView/userinterface/templatetags/markdown.py
+++ DevOpsCustomerView/userinterface/templatetags/markdown.py
@@ -9,25 +9,37 @@ import re
 register = Library()
 
 
+#customTranslations = {
+#    "(^<p>|</p>$)": "<br>",
+#    "\n": "<br>",
+#    r"\[ \] -": '<input type="checkbox" disabled>',
+#    r"\[x\] -": '<input type="checkbox" checked disabled>',
+#    r"\[ \]": '<input type="checkbox" disabled>',
+#    r"\[x\]": '<input type="checkbox" checked disabled>',
+#    '<ul>': '<ul class="list-group">',
+#    '<br><li>': '<li>',
+#    '<li>': '<li class="list-group-item">',
+#}
+
+# only convert checkbox
 customTranslations = {
-    "(^<p>|</p>$)": "<br>",
-    "\n": "<br>",
     r"\[ \] -": '<input type="checkbox" disabled>',
     r"\[x\] -": '<input type="checkbox" checked disabled>',
     r"\[ \]": '<input type="checkbox" disabled>',
     r"\[x\]": '<input type="checkbox" checked disabled>',
-    '<ul>': '<ul class="list-group">',
-    '<br><li>': '<li>',
-    '<li>': '<li class="list-group-item">',
+    '<table>' : '<table class="table">'
+    #'<ul>': '<ul class="list-group">',
+    #'<li>': '<li class="list-group-item">'
 }
 
 @register.filter()
 def markdown(text):
-        
-    text = md.markdown(text,extensions=['fenced_code', 'nl2br'])
-    for key,value in customTranslations.items():
+    print(text)
+    text = md.markdown(text,extensions=['fenced_code', 'nl2br','markdown.extensions.tables','pymdownx.tilde'])
+    print(text)
+    for key,value in customTranslations.items():          
         text = re.sub(key,value,text)
-
+    print(text)
     for employee in TeamMember.objects.all():
         text = text.replace('@'+employee.username,'%s').replace('%s',employee.name)
         text = text.replace(employee.username,'%s').replace('%s',employee.name)
