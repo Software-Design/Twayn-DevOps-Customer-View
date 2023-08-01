10034bc4024cfa98b7fbd9b496ce9bb99fe0927c
Daniel Kemen <dk@software-design.de>
2023-08-01 08:30:08 +0200

    - Potential fix

--- DevOpsCustomerView/userinterface/views.py
+++ DevOpsCustomerView/userinterface/views.py
@@ -450,7 +450,7 @@ def printOverview(request: WSGIRequest, slug: str, id:  int, date: str):
         return glProject
 
     projectIdentifier = glProject['localProject'].projectIdentifier
-    pdfkit.from_string(template('print/overview').render(glProject|{'issues': glProject['remoteProject'].issues.list(updated_after=datetime.datetime.strptime(date,'%Y-%m-%d'))}, request), '/tmp/'+projectIdentifier+'.pdf', {'encoding': 'UTF-8', '--footer-center': '[page] '+_(
+    pdfkit.from_string(template('print/overview').render(glProject|{'issues': glProject['remoteInstance'].issues.list(updated_after=datetime.datetime.strptime(date,'%Y-%m-%d'))}, request), '/tmp/'+projectIdentifier+'.pdf', {'encoding': 'UTF-8', '--footer-center': '[page] '+_(
         'of')+' [topage]', '--footer-left': settings.INTERFACE_NAME, '--footer-right': datetime.datetime.now().strftime('%d.%m.%Y')}, verbose=True)
 
     with open('/tmp/'+projectIdentifier+'.pdf', 'rb') as f:
